66a7a8b8c5ecddc62b351da208cddd1103c98769
Björn Höfling <bjoern.hoefling@bjoernhoefling.de>
2020-09-29 09:34:01 +0200

    App.java: Adding spaces, breaking the linter.

--- src/main/java/de/bjoernhoefling/App.java
+++ src/main/java/de/bjoernhoefling/App.java
@@ -14,7 +14,7 @@ public final class App {
   /** Main method.
   @param args Command line arguments.
   */
-  public static void main(final String[] args) {
+  public static void main( final String[] args ) {
     System.out.println("Hello World!");
   }
 }
